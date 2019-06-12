339f88e88a6ba14577a6d6ef54b5c7d30a59e3d1
Steve Murphy <stephmur@gmail.com>
2019-06-12 15:32:06 -0600

    Initial Commit

---
+++ ALittleTODO/Document.swift
@@ -0,0 +1,22 @@
+//
+//  Document.swift
+//  ALittleTODO
+//
+//  Created by Stephen Murphy on 6/12/19.
+//  Copyright © 2019 Stephen C. Murphy. All rights reserved.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
